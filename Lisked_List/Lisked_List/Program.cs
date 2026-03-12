b7c68dda72c168b86ac52e2a1985f34ebb62f027
mostapha <mostaphabotte@gmail.com>
2026-03-12 01:24:05 +0000

    implementing doubly linked list from scratch

--- Lisked_List/Lisked_List/Program.cs
+++ Lisked_List/Lisked_List/Program.cs
@@ -32,18 +32,18 @@ namespace Lisked_List
     }
     class Node<T>
     {
-        public T? Data = default(T);
+        public T? Data = default;
         public Node<T>? Next = null;
         public Node<T>? Previous = null;
-        internal DoublyLinkedList<T> _List;
+        internal DoublyLinkedList<T>? _List;
 
-        public Node(T? data, DoublyLinkedList<T> list)
+        public Node(T data, DoublyLinkedList<T> list)
         {
             Data = data;
             _List = list;
         }
     }
-    class DoublyLinkedList<T>
+     class DoublyLinkedList<T> : IEnumerable<T> 
     {
         private Node<T>? _Head = null;
         private Node<T>? _Tail = null;
@@ -133,8 +133,80 @@ namespace Lisked_List
             return false;
         }
 
+        public bool Remove(T Value)
+       {
+
+            Node<T>? temp = _Head;
+            while (temp != null)
+            {
+                if(temp.Data!=null && temp.Data.Equals(Value))
+                {
+                   
+
+                    Node<T>? next = temp.Next;
+                    Node<T>? Previous = temp.Previous;
+
+                    if(next !=null)
+                    {
+                        next.Previous = Previous;
+                    }
+                    else
+                    {
+                        _Tail = Previous;
+                    }
+
+                    if(Previous !=null)
+                    {
+                        Previous.Next = temp.Next;
+                    }
+                    else
+                    {
+                        _Head = next;
+                    }
+                    temp._List = null;
+                    temp.Next = null;
+                    temp.Previous = null;
+                    temp = null;
+                    return true;
+                }
+                temp = temp.Next;
+            }
+            return false;
+        }
+
+        public bool Remove(Node<T> Node)
+        {
+              if (Node == null ||  Node._List != this ) return false;
 
 
+              Node<T>? next = Node.Next;
+              Node<T>? Previous = Node.Previous;
+              
+              if (next != null)
+              {
+                  next.Previous = Previous;
+              }
+              else
+              {
+                  _Tail = Previous;
+              }
+              
+              if (Previous != null)
+              {
+                  Previous.Next = Node.Next;
+              }
+              else
+              {
+                  _Head = next;
+              }
+              Node._List = null;
+              Node.Next = null;
+              Node.Previous = null;
+              return true;
+       
+          
+        }
+        
         public Node<T>? GetNode(T Value)
         {
                 Node<T>? temp = _Head;
@@ -154,6 +226,7 @@ namespace Lisked_List
                 }
                 return null;        
         }
+
         public void PrintBackWard()
         {
             if(_Tail==null)
@@ -194,11 +267,24 @@ namespace Lisked_List
 
         }
 
-
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T>? temp = _Head;
+            while (temp != null)
+            {
+                yield return temp.Data;
+                temp = temp.Next;
+            }
+        }
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
 
     }
     internal class Program
     {
+        
         static void Main(string[] args)
         {
             DoublyLinkedList<TrainStation> TrainStations = new DoublyLinkedList<TrainStation>();
@@ -216,7 +302,7 @@ namespace Lisked_List
             TrainStations.PrintForward();
             TrainStations.PrintBackWard();
 
-            Node<TrainStation>? node = TrainStations.GetNode(TrainStationA);
+            Node<TrainStation>? node = TrainStations.GetNode(TrainStationC);
             Console.WriteLine("The Node is  :"+node?.Data);
             Console.WriteLine("The next Node is :"+node?.Next?.Data);
             Console.WriteLine("The previous Node is :" + node?.Previous?.Data);
@@ -224,19 +310,28 @@ namespace Lisked_List
 
             TrainStation TrainStationC1 = new TrainStation("C1");
 
+            TrainStations.InsertBefore(node, TrainStationC1);
 
-            DoublyLinkedList<TrainStation> TrainStations2 = new DoublyLinkedList<TrainStation>();
-            TrainStations2.AddFirst(TrainStationA);
-            Node<TrainStation>? node2 = TrainStations2.GetNode(TrainStationA);
+            TrainStations.PrintForward();
 
+            TrainStations.Remove(TrainStationB);
 
-            TrainStations.InsertBefore(node2, TrainStationC1);
-
+            Console.WriteLine("After removing By Value ");
             TrainStations.PrintForward();
             TrainStations.PrintBackWard();
 
 
+            TrainStations.Remove(node);
 
+            Console.WriteLine("After removing By node ");
+            TrainStations.PrintForward();
+            TrainStations.PrintBackWard();
+
+            Console.WriteLine("Iterate the linked list");
+            foreach (TrainStation item in TrainStations)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
