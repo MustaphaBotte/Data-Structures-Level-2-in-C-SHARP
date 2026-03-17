6c9a393b5f421e196b5a7646f31e1af7ad75c16a
mostapha <mostaphabotte@gmail.com>
2026-03-17 23:29:01 +0000

    ICollection & IList interfaces implementation

---
+++ IList/iList/Program.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+namespace iList
+{
+    class CustomCollection<T> : IList<T>
+    {
+        private List<T> _list = new List<T>();
+
+        public int Count => _list.Count;
+
+        public T this[int index]
+        {
+            set
+            {
+                if (index < _list.Count)
+                {
+                    this._list[index] = value;
+                }
+            }
+            get
+            {
+                return this._list[index];
+            }
+        }
+
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+        public int IndexOf(T item)
+        {
+            return _list.IndexOf(item);
+        }
+        public void Insert(int index, T item)
+        {
+            _list.Insert(index, item);
+        }
+        public void RemoveAt(int index)
+        {
+            _list.RemoveAt(index);
+        }
+        public bool Contains(T item)
+        {
+            return _list.Contains(item);
+        }
+        public bool Remove(T item)
+        {
+            return _list.Remove(item);
+        }
+        public void CopyTo(T[] destination, int ArrayIndex)
+        {
+            _list.CopyTo(destination, ArrayIndex);
+        }
+        public void Clear()
+        {
+            this._list.Clear();
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _list.Count; i++)
+            {
+                yield return _list[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        public void Add(T item)
+        {
+            _list.Add(item);
+        }
+        // i used List<T> here
+        // you can use any other DS you want , but you need to implement the interface 
+        // now you can extend your collection
+   
+        }
+
+       internal class Program
+       {
+          static void Main(string[] args)
+          {
+            CustomCollection<int> collection = new CustomCollection<int>() { 10, 20, 30, 40 };
+           
+            foreach (var item in collection)
+            {
+               Console.WriteLine(item);
+            }
+            Console.WriteLine("Collection Count :" + collection.Count);
+            Console.WriteLine("is Read Only  :" + collection.IsReadOnly);
+            collection.Add(50);
+            collection.Remove(10);
+            Console.WriteLine("Final items :");
+            foreach (var item in collection)
+            {
+               Console.WriteLine(item);
+            }
+           
+            collection.Insert(0, 1);
+            int index = collection.IndexOf(30);
+            Console.WriteLine("Index of 30 " + index);
+            collection[1] = 58;
+            collection.RemoveAt(collection.Count - 1);
+
+
+            Console.WriteLine("Final items :");
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+
+
+           Type t = collection.GetType();
+            foreach (var item in t.GetMethods())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+        
